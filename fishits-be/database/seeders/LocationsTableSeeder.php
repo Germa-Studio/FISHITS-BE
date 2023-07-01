ff1eb641544d4413ca527ee1cd61a76078308844
okyanawang <okyanawang@gmail.com>
2023-07-01 09:25:16 +0700

    Negative the latitude

--- fishits-be/database/seeders/LocationsTableSeeder.php
+++ fishits-be/database/seeders/LocationsTableSeeder.php
@@ -25,7 +25,7 @@ class LocationsTableSeeder extends Seeder
             array (
                 'id' => 1,
                 'name' => 'Dorang 1',
-                'latitude' => 6.656116667,
+                'latitude' => -6.656116667,
                 'longitude' => 113.8387333,
                 's' => 39.367,
                 'e' => 50.324,
@@ -36,7 +36,7 @@ class LocationsTableSeeder extends Seeder
             array (
                 'id' => 2,
                 'name' => 'Ikan Merah',
-                'latitude' => 6.697433333,
+                'latitude' => -6.697433333,
                 'longitude' => 113.6780,
                 's' => 41.846,
                 'e' => 40.68,
@@ -47,7 +47,7 @@ class LocationsTableSeeder extends Seeder
             array (
                 'id' => 3,
                 'name' => 'MD 1',
-                'latitude' => 6.520616667,
+                'latitude' => -6.520616667,
                 'longitude' => 113.7680,
                 's' => 31.237,
                 'e' => 46.081,
@@ -58,7 +58,7 @@ class LocationsTableSeeder extends Seeder
             array (
                 'id' => 4,
                 'name' => 'MD 2',
-                'latitude' => 6.518750,
+                'latitude' => -6.518750,
                 'longitude' => 113.7810833,
                 's' => 31.125,
                 'e' => 46.865,
@@ -69,7 +69,7 @@ class LocationsTableSeeder extends Seeder
             array (
                 'id' => 5,
                 'name' => 'Tos Pote',
-                'latitude' => 6.670616667,
+                'latitude' => -6.670616667,
                 'longitude' => 113.7592,
                 's' => 40.237,
                 'e' => 45.552,
@@ -80,7 +80,7 @@ class LocationsTableSeeder extends Seeder
             array (
                 'id' => 6,
                 'name' => 'Zam 1',
-                'latitude' => 6.695233333,
+                'latitude' => -6.695233333,
                 'longitude' => 113.9754833,
                 's' => 41.714,
                 'e' => 58.529,
@@ -91,7 +91,7 @@ class LocationsTableSeeder extends Seeder
             array (
                 'id' => 7,
                 'name' => 'Tompas 1',
-                'latitude' => 6.610833333,
+                'latitude' => -6.610833333,
                 'longitude' => 113.8947333,
                 's' => 36.650,
                 'e' => 53.684,
@@ -102,7 +102,7 @@ class LocationsTableSeeder extends Seeder
             array (
                 'id' => 8,
                 'name' => 'Tos 2',
-                'latitude' => 6.658350,
+                'latitude' => -6.658350,
                 'longitude' => 113.7708667,
                 's' => 39.501,
                 'e' => 46.252,
@@ -113,7 +113,7 @@ class LocationsTableSeeder extends Seeder
             array (
                 'id' => 9,
                 'name' => 'TLI',
-                'latitude' => 6.694350,
+                'latitude' => -6.694350,
                 'longitude' => 113.97535,
                 's' => 41.661,
                 'e' => 58.521,
@@ -124,7 +124,7 @@ class LocationsTableSeeder extends Seeder
             array (
                 'id' => 10,
                 'name' => 'TAAA 5',
-                'latitude' => 6.593900,
+                'latitude' => -6.593900,
                 'longitude' => 113.9062,
                 's' => 35.634,
                 'e' => 54.371,
@@ -135,7 +135,7 @@ class LocationsTableSeeder extends Seeder
             array (
                 'id' => 11,
                 'name' => 'SMBR 1',
-                'latitude' => 6.608100,
+                'latitude' => -6.608100,
                 'longitude' => 113.85015,
                 's' => 36.486,
                 'e' => 51.009,
@@ -146,7 +146,7 @@ class LocationsTableSeeder extends Seeder
             array (
                 'id' => 12,
                 'name' => 'SMBR 3',
-                'latitude' => 6.569750,
+                'latitude' => -6.569750,
                 'longitude' => 113.84155,
                 's' => 34.185,
                 'e' => 50.493,
@@ -157,7 +157,7 @@ class LocationsTableSeeder extends Seeder
             array (
                 'id' => 13,
                 'name' => 'TADEZ 4',
-                'latitude' => 6.384633,
+                'latitude' => -6.384633,
                 'longitude' => 113.8952167,
                 's' => 23.078,
                 'e' => 53.713,
@@ -168,7 +168,7 @@ class LocationsTableSeeder extends Seeder
             array (
                 'id' => 14,
                 'name' => 'TADEZ 5',
-                'latitude' => 6.521033,
+                'latitude' => -6.521033,
                 'longitude' => 113.87125,
                 's' => 31.262,
                 'e' => 52.275,
@@ -179,7 +179,7 @@ class LocationsTableSeeder extends Seeder
             array (
                 'id' => 15,
                 'name' => 'KETENG',
-                'latitude' => 6.588833,
+                'latitude' => -6.588833,
                 'longitude' => 113.6960667,
                 's' => 35.330,
                 'e' => 41.764,
