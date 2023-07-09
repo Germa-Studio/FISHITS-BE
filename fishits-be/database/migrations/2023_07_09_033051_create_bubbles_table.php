7b1e7b8518b1f11acf7f3317d7332bb00b9ae8b2
fillahaufi <fillahaufi140101@gmail.com>
2023-07-09 10:33:04 +0700

    Bubble seeder by @okyan done

---
+++ fishits-be/database/migrations/2023_07_09_033051_create_bubbles_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bubbles', function (Blueprint $table) {
+            $table->id();
+            $table->double('latitude', 10, 6);
+            $table->double('longitude', 10, 6);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bubbles');
+    }
+};
